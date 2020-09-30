10c5369333fae5369b709328af8cb9b0aa299f69
N.Pantiukhova <NataliaPantiukhova@users.noreply.github.com>
2020-09-30 12:41:03 +0300

    feat: resolve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let resultArr = [],
+      pairOfBrackets = {},
+      arrayFromString = str.split('');
+
+  for(let i = 0; i < bracketsConfig.length; i++) {
+    pairOfBrackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+
+  for(let i = 0; i < arrayFromString.length; i++) {
+    if(arrayFromString[i] == (resultArr[resultArr.length - 1] || 0)) {
+      resultArr.pop();
+    } else {
+      resultArr.push(pairOfBrackets[arrayFromString[i]]);
+    }
+
+  }
+  if(resultArr.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
